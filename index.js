ac773c8255f47e19f91f3549f860732f3880dea0
kamal98ETH <bestkacm@gmail.com>
2022-11-03 22:44:54 +0100

    feat: complete fcc challenges

--- index.js
+++ index.js
@@ -11,10 +11,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
+});
+
+//my app
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
 });
